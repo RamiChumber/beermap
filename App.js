e17f5e4a023bbf8ac644f46acc9ca66db2dacbcf
trucar00 <98335830+trucar00@users.noreply.github.com>
2025-06-03 21:06:10 +0200

    Shows a custom styled mapbox map.

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import Mapbox, {MapView} from "@rnmapbox/maps";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+Mapbox.setAccessToken("pk.eyJ1IjoidHJ1Y2FyIiwiYSI6ImNtYmF4eW1kejBrbngyanNiMDV6cmhkZGUifQ.XDN6-TsN4oQ8jhiwd1Oguw");
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
+  container: {
+    height: windowHeight,
+    width: windowWidth,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
 });
+
+export default class App extends Component {
+  componentDidMount() {
+    Mapbox.setTelemetryEnabled(false);
+  }
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            styleURL="mapbox://styles/trucar/cmbfonisj006l01r03k4d03e7"
+            compassEnabled
+            compassFadeWhenNorth
+          >
+            <Mapbox.Camera
+              zoomLevel={12}
+              centerCoordinate={[10.7522, 59.9139]}
+            />
+          </MapView>
+        </View>
+      </View>
+    );
+  }
+}
