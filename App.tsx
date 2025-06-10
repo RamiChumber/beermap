d7542c669d982db2e397b9a79ac169f492a46edc
trucar00 <98335830+trucar00@users.noreply.github.com>
2025-06-10 14:26:19 +0200

    Trying to get oslo bars showing on the map from generated geojson file

--- App.tsx
+++ App.tsx
@@ -11,55 +11,16 @@ Mapbox.setAccessToken("pk.eyJ1IjoidHJ1Y2FyIiwiYSI6ImNtYmF4eW1kejBrbngyanNiMDV6cm
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const geojson: GeoJSON.FeatureCollection = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-77.032, 38.913]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'Washington, D.C.'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-122.414, 37.776]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'San Francisco, California'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-122.614, 37.876]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'San Francisco, California'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-122.214, 37.676]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'San Francisco, California'
-      }
-    },
-  ]
-};
+const geojson: GeoJSON.FeatureCollection = 
+  {
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "element": "node", "id": 288416026, "amenity": "bar", "check_date": "2023-06-03", "name": "Svanen Cocktailbar", "outdoor_seating": "yes", "toilets:wheelchair": "yes", "website": "https://www.svanenoslo.no", "wheelchair": "no", "contact:instagram": null, "indoor_seating": null, "opening_hours:signed": null, "opening_hours": null, "fax": null, "phone": null, "noname": null, "not:name": null, "description": null, "brewery": null, "changing_table": null, "drink:beer": null, "drink:cocktail": null, "drink:whisky": null, "drink:wine": null, "toilets": null, "toilets:access": null, "toilets:disposal": null, "toilets:gender_segregated": null, "toilets:hands_drying": null, "toilets:handwashing": null, "toilets:paper_supplied": null, "contact:facebook": null, "cuisine": null, "internet_access": null, "internet_access:fee": null, "contact:email": null, "contact:phone": null, "check_date:opening_hours": null, "microbrewery": null, "min_age": null, "payment:app": null, "payment:cards": null, "payment:cash": null, "payment:credit_cards": null, "payment:debit_cards": null, "smoking": null, "email": null, "twitter": null, "fixme": null, "name:en": null, "note": null, "source": null, "diet:vegan": null, "diet:vegetarian": null, "image": null, "operator": null, "live_music": null, "level": null, "cocktails": null, "facebook": null, "website:menu": null, "lgbtq": null, "wikidata": null, "wikipedia": null, "club": null, "sport": null, "addr:housenumber": null, "addr:street": null, "addr:postcode": null, "start_date": null, "bar": null, "indoor": null, "type": null, "building": null, "building:levels": null, "ref:bygningsnr": null, "roof:shape": null }, "geometry": { "type": "Point", "coordinates": [ 10.7447916, 59.9123665 ] } },
+{ "type": "Feature", "properties": { "element": "node", "id": 292135704, "amenity": "bar", "check_date": "2023-02-19", "name": "Parkteateret", "outdoor_seating": null, "toilets:wheelchair": null, "website": null, "wheelchair": "no", "contact:instagram": null, "indoor_seating": null, "opening_hours:signed": null, "opening_hours": null, "fax": null, "phone": null, "noname": null, "not:name": null, "description": null, "brewery": null, "changing_table": null, "drink:beer": null, "drink:cocktail": null, "drink:whisky": null, "drink:wine": null, "toilets": null, "toilets:access": null, "toilets:disposal": null, "toilets:gender_segregated": null, "toilets:hands_drying": null, "toilets:handwashing": null, "toilets:paper_supplied": null, "contact:facebook": null, "cuisine": null, "internet_access": null, "internet_access:fee": null, "contact:email": null, "contact:phone": null, "check_date:opening_hours": null, "microbrewery": null, "min_age": null, "payment:app": null, "payment:cards": null, "payment:cash": null, "payment:credit_cards": null, "payment:debit_cards": null, "smoking": null, "email": null, "twitter": null, "fixme": null, "name:en": null, "note": null, "source": null, "diet:vegan": null, "diet:vegetarian": null, "image": null, "operator": null, "live_music": null, "level": null, "cocktails": null, "facebook": null, "website:menu": null, "lgbtq": null, "wikidata": null, "wikipedia": null, "club": null, "sport": null, "addr:housenumber": null, "addr:street": null, "addr:postcode": null, "start_date": null, "bar": null, "indoor": null, "type": null, "building": null, "building:levels": null, "ref:bygningsnr": null, "roof:shape": null }, "geometry": { "type": "Point", "coordinates": [ 10.7583654, 59.9234961 ] } },
+{ "type": "Feature", "properties": { "element": "node", "id": 292143391, "amenity": "bar", "check_date": "2024-07-15", "name": "Jimmy’s", "outdoor_seating": "yes", "toilets:wheelchair": null, "website": null, "wheelchair": "no", "contact:instagram": "https://www.instagram.com/jimmysoslo/", "indoor_seating": "yes", "opening_hours:signed": null, "opening_hours": null, "fax": null, "phone": null, "noname": null, "not:name": null, "description": null, "brewery": null, "changing_table": null, "drink:beer": null, "drink:cocktail": null, "drink:whisky": null, "drink:wine": null, "toilets": null, "toilets:access": null, "toilets:disposal": null, "toilets:gender_segregated": null, "toilets:hands_drying": null, "toilets:handwashing": null, "toilets:paper_supplied": null, "contact:facebook": null, "cuisine": null, "internet_access": null, "internet_access:fee": null, "contact:email": null, "contact:phone": null, "check_date:opening_hours": null, "microbrewery": null, "min_age": null, "payment:app": null, "payment:cards": null, "payment:cash": null, "payment:credit_cards": null, "payment:debit_cards": null, "smoking": null, "email": null, "twitter": null, "fixme": null, "name:en": null, "note": null, "source": null, "diet:vegan": null, "diet:vegetarian": null, "image": null, "operator": null, "live_music": null, "level": null, "cocktails": null, "facebook": null, "website:menu": null, "lgbtq": null, "wikidata": null, "wikipedia": null, "club": null, "sport": null, "addr:housenumber": null, "addr:street": null, "addr:postcode": null, "start_date": null, "bar": null, "indoor": null, "type": null, "building": null, "building:levels": null, "ref:bygningsnr": null, "roof:shape": null }, "geometry": { "type": "Point", "coordinates": [ 10.7575314, 59.9192355 ] } },
+{ "type": "Feature", "properties": { "element": "node", "id": 292143395, "amenity": "bar", "check_date": "2025-05-10", "name": "Bar Boca", "outdoor_seating": null, "toilets:wheelchair": null, "website": "https://www.facebook.com/BarBocaOslo", "wheelchair": "no", "contact:instagram": null, "indoor_seating": null, "opening_hours:signed": "no", "opening_hours": null, "fax": null, "phone": null, "noname": null, "not:name": null, "description": null, "brewery": null, "changing_table": null, "drink:beer": null, "drink:cocktail": null, "drink:whisky": null, "drink:wine": null, "toilets": null, "toilets:access": null, "toilets:disposal": null, "toilets:gender_segregated": null, "toilets:hands_drying": null, "toilets:handwashing": null, "toilets:paper_supplied": null, "contact:facebook": null, "cuisine": null, "internet_access": null, "internet_access:fee": null, "contact:email": null, "contact:phone": null, "check_date:opening_hours": null, "microbrewery": null, "min_age": null, "payment:app": null, "payment:cards": null, "payment:cash": null, "payment:credit_cards": null, "payment:debit_cards": null, "smoking": null, "email": null, "twitter": null, "fixme": null, "name:en": null, "note": null, "source": null, "diet:vegan": null, "diet:vegetarian": null, "image": null, "operator": null, "live_music": null, "level": null, "cocktails": null, "facebook": null, "website:menu": null, "lgbtq": null, "wikidata": null, "wikipedia": null, "club": null, "sport": null, "addr:housenumber": null, "addr:street": null, "addr:postcode": null, "start_date": null, "bar": null, "indoor": null, "type": null, "building": null, "building:levels": null, "ref:bygningsnr": null, "roof:shape": null }, "geometry": { "type": "Point", "coordinates": [ 10.7593699, 59.9252056 ] } },
+]
+}
 
 const styles = StyleSheet.create({
   page: {
@@ -128,6 +89,7 @@ const App = () => {
                 // Optional: Zoom into cluster
                 zoomInCluster(e.coordinates);
               } else {
+                console.log(e.features)
                 setSelectedFeature(feature);
               }
             }} 
